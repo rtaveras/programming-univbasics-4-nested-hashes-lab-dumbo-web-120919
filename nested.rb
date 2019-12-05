1eb8d15f4379bbcbde2a033444c618599528dfff
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 06:57:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,21 @@ def hopper
 end
 
 def alan_kay_is_known_for
-          "Object Orientation"
+          	programmer_hash = 
+		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 end
 
 def dennis_ritchies_language

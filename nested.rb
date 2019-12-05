11fd296494f846561cbaf34b015da4b1006b582b
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 06:35:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -5,7 +5,6 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-          return hopper
         },
         :alan_kay => {
           :known_for => "Object Orientation",

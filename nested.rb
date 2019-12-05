652359620a6e5c29076cbcb2bf13a7269ed58ff3
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 06:45:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -9,10 +9,8 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	programmer_hash = 
- 		{
+	programmer_hash = {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
      }

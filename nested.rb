72fcf90fecf48ea22dfea03c4947a9a5ec6fcbb8
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 06:30:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,7 +6,15 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+        :grace_hopper
      }
 
 
